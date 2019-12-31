a876a386d041e3b69d6270eae1a795613068dbf0
Sonal Kharade <sonal.k@corp.codewalla.com>
2019-12-31 17:56:29 +0530

    Create Contest Code Complete

--- src/main/java/com/api/qa/pages/RouterAPI.java
+++ src/main/java/com/api/qa/pages/RouterAPI.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertEquals;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 import org.eclipse.persistence.sessions.serializers.JSONSerializer;
 import org.json.JSONArray;
@@ -33,14 +34,15 @@ import io.restassured.specification.RequestSpecification;
 
 public class RouterAPI {
 	
-
+	String token;
 
 	@BeforeMethod
 	public void setup() {
 
 	}
+	
 
-	@Test(enabled=true)
+	@Test(priority=1)
 	public void Login_Post() throws ParseException {
 		
 		//Created by sonal	
@@ -52,8 +54,8 @@ public class RouterAPI {
 		req.header("api-secret", "3ca09dc649ac5ec61e1a6c0fc17d5819UNia1nirLrBbcVxwSg52wjzrt6yjeHESIGAPwPID");
 
 		JSONObject json = new JSONObject();
-		json.put("email", "managertest17@gmail.com");
-		json.put("password", "Test@123");
+		json.put("email", "apiautomationmanager@gmail.com");
+		json.put("password", "pass");
 
 		req.body(json.toJSONString());
 
@@ -62,51 +64,68 @@ public class RouterAPI {
 		ResponseBody body=response.getBody();
 		String responseData=body.asString();
 		
-		System.out.println("Response Body: "+responseData);
-		System.out.println("Status Code: " + statuscode);
+		System.out.println("Response Body for Login: "+responseData);		
 		
 		JSONParser jsonParser = new JSONParser();
-		try {
+		//try {
 			JSONObject jsonObject = (JSONObject) jsonParser.parse(responseData);
 			JSONObject data= (JSONObject) jsonObject.get("data");
 			//JSONObject manager= (JSONObject) data.get("manager");
 			JSONObject authentication= (JSONObject) data.get("authentication");
-			String token=(String) authentication.get("onehuddletoken");
-			System.out.println("Authentication Token: "+token);
+			token=(String) authentication.get("onehuddletoken");
+			//System.out.println("Authentication Token: "+token);
+			String message=(String) jsonObject.get("message");
+			Boolean success=(Boolean) jsonObject.get("success");
 			
-		} catch (ParseException e) {
+		//} catch (ParseException e) {
 			 //TODO Auto-generated catch block
-			e.printStackTrace();
-		}					
+			//e.printStackTrace();
+		//}					
 		Assert.assertEquals(statuscode, 200);
+		//Assert.assertEquals(success, true);
+		Assert.assertTrue(success);
+		Assert.assertEquals(message, "authenticated.");
+		
 	}
-	@Test(enabled=false)
-	public void CreateContest_POST()
+	@Test(priority=2)
+	public void CreateContest_POST() throws ParseException
 	{
 		RequestSpecification req = RestAssured.given();
 		req.header("Content-Type", "application/json");
 		req.header("accept", "application/json");
-		req.header("session-token","5e2961e15a782bc15532274481dfe5b7Hpta8TYCxD3Qr5R34IxheJh2HwbRftoaRL8oBu7H");	
+		req.header("session-token",token);	
 		 LocalDateTime startDate = LocalDateTime.now().plusMinutes(2);
 		 LocalDateTime endDate = LocalDateTime.now().plusMinutes(5);
 	     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
 		
 		JSONObject json = new JSONObject();
+		Random rand= new Random();
+		int no=rand.nextInt(1000);
 		
-		json.put("company_id", "1183");
-		json.put("contest_name", "Test Contest");
+		json.put("company_id", "1201");
+		json.put("contest_name", "Test Contest-" + no);
 		json.put("contest_start_date", "" + startDate.format(formatter) + "");
 		json.put("contest_end_date", "" + endDate.format(formatter) + "");
-		System.out.println(json);
 		
 		req.body(json.toJSONString());
 		Response response=req.post("https://dev-api.1huddle.co/api/rest/v2.0/contest/add");
 		int statuscode= response.getStatusCode();
 		ResponseBody body=response.getBody();
-
-		System.out.println("Response Body: "+body.asString());
-		System.out.println("Status Code: " + statuscode);
+		String responseData=body.asString();
+		
+		System.out.println("Response Body for Create Contest: " + responseData);		
+		
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) jsonParser.parse(responseData);
+		String message=(String) jsonObject.get("message");
+		Boolean success=(Boolean) jsonObject.get("success");
+		
+		Assert.assertEquals(statuscode, 200);
+		//Assert.assertEquals(success, "true");
+		Assert.assertTrue(success);
+		Assert.assertEquals(message, "Contest added successfully.");
+		
 		
 	}
 
